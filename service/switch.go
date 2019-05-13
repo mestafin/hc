9af6414a7503ffee15e2a68f1f89af9e1caa759f
mestafin <gitkraken@mestafin.com>
2019-05-13 19:27:06 +0200

    added Brighness Characteristic to Switch Service

--- service/switch.go
+++ service/switch.go
@@ -1,4 +1,5 @@
 // THIS FILE IS AUTO-GENERATED
+// mestafin added
 package service
 
 import (
@@ -11,6 +12,7 @@ type Switch struct {
 	*Service
 
 	On *characteristic.On
+	Brightness *characteristic.Brightness
 }
 
 func NewSwitch() *Switch {
@@ -19,6 +21,10 @@ func NewSwitch() *Switch {
 
 	svc.On = characteristic.NewOn()
 	svc.AddCharacteristic(svc.On.Characteristic)
+
+	// mestafin added
+	svc.Brightness = characteristic.NewBrightness()
+	svc.AddCharacteristic(svc.Brightness.Characteristic)
 
 	return &svc
 }
